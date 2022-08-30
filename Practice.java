b65b9213fbdcd4e2382c025a281017d3366bbb87
kesjuan <kesjuan@gmail.com>
2022-08-30 17:45:23 -0400

    practiceHW

---
+++ Practice.java
@@ -0,0 +1,69 @@
+// created class practice
+public class Practice {
+        // create a Main method psvm shortcut
+    public static void main(String[] args) {
+        // print out a string with print method
+        System.out.println("Yellow");
+        // declaring variables
+        int height;
+        String address;
+        // declaring multiple variables with different data types
+        //created string variable first name assigned value as Jared. string is a text value
+        String firstName = "Jared";
+        // created int variable age assigned value 25. int is a whole number
+        int age = 25;
+        // created float variable shoesize assigned value 10.5. float is a decimal number
+        float shoeSize = 10.5f;
+        //created char variable sex assigned value M. char is a single character
+        char sex = 'M';
+        // created boolean student variable assigned true. boolean is true or false
+        boolean student = true;
+        // use print method to print variables
+        System.out.println(firstName);
+        System.out.println(age);
+        // a few more number data types
+        // byte stores whole numbers from -128 to 127
+        byte accBalance = -80;
+        // long stores a wider range of whole numbers negative to positive
+        long earthAge = 9000000;
+        // double stores fractional numbers up to 15 decimal digits
+        double pie = 3.14159265359;
+               //
+        // use ^^ to create single line comments inside code
+
+        //use /* */ for multi line code
+        /*
+        for
+        example
+         */
+        // using variables for math
+        int a = 2;
+        int b = 90;
+
+        System.out.println(a*b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
